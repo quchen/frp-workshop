24ee284f2f8004d707b3511aaeae72c1ad080ef9
David Luposchainsky <dluposchainsky@gmail.com>
2017-03-22 14:16:31 +0100

    Minimal only-somewhat-broken slippery paddle version :-)

--- pong/app/Main.hs
+++ pong/app/Main.hs
@@ -7,15 +7,13 @@ module Main where
 
 import Control.Concurrent
 import Control.Concurrent.Async
+import Control.Exception
 import Control.Lens
 import Control.Monad
 import Data.Function
-import Data.Monoid
 import Graphics.Vty               as Vty
 import Reactive.Banana            as Frp
 import Reactive.Banana.Frameworks as Frp
-import System.IO
-
 
 
 
@@ -25,12 +23,16 @@ data Player = Player
     }
 
 data Ball = Ball
-    { _position :: Vec2
-    , _velocity :: Vec2 }
+    { _position :: Vec2Cart
+    , _velocity :: Vec2Rad }
 
-data Vec2 = Vec2
+data Vec2Cart = Vec2Cart
     { _x :: Double
     , _y :: Double }
+
+data Vec2Rad = Vec2Rad
+    { _r   :: Double
+    , _phi :: Double }
 
 data GameState = GameState
     { _leftPlayer  :: Player
@@ -40,80 +42,92 @@ data GameState = GameState
 
 makeLenses ''Player
 makeLenses ''Ball
-makeLenses ''Vec2
+makeLenses ''Vec2Cart
+makeLenses ''Vec2Rad
 makeLenses ''GameState
 
 renderToTerminal :: GameState -> IO ()
 renderToTerminal = error "TODO: renderToTerminal"
 
 makeNetworkDescription
-    :: AddHandler Char -- ^ User input
-    -> AddHandler ()   -- ^ Clock tick
+    :: Vty                  -- ^ Vty to render to
+    -> AddHandler Vty.Event -- ^ User input
+    -> AddHandler ()        -- ^ Clock tick
     -> MomentIO ()
-makeNetworkDescription addKeyEvent addClockTickEvent = do
-    eKey <- fromAddHandler addKeyEvent
+makeNetworkDescription vty addVtyEvent addClockTickEvent = do
+    eVty <- fromAddHandler addVtyEvent
 
     eTime <- do
         eTick <- fromAddHandler addClockTickEvent
         accumE (0 :: Int) (fmap (\_ time -> time + 1) eTick)
 
     bGameState <- do
-        bTime <- stepper 0 eTime
-        let eUpdateGameState = (,) <$> bTime <@> eKey
+        let eKey = flip fmap eVty (\case
+                EvKey key modifiers -> Just (key, modifiers)
+                _other -> Nothing
+                )
+        bKey <- stepper Nothing eKey
+        let eUpdateGameState = (,) <$> bKey <@> eTime
         accumB initialGameState (fmap updateGameState eUpdateGameState)
 
     let eRender = bGameState <@ eTime
 
-    reactimate (fmap (\time -> putStrLn ("Time: " ++ show time)) eTime)
-    reactimate (fmap (\key -> putStrLn ("Key press: " ++ show key)) eKey)
-    reactimate (fmap (\_gs -> putStrLn "Rendering frame!") eRender)
+    -- reactimate (fmap (\_ -> putStrLn "tick") eTime)
+    reactimate (flip fmap eRender (update vty . render))
 
-updateGameState :: (Int, Char) -> GameState -> GameState
-updateGameState (_time, key) s = error "Update game state" key s
+mapMaybe :: (a -> Maybe b) -> Frp.Event a -> Frp.Event b
+mapMaybe f es = filterJust (fmap f es)
+
+updateGameState :: (Maybe (Vty.Key, [Vty.Modifier]), time) -> GameState -> GameState
+updateGameState = \case
+    (Nothing, _time) -> moveBall
+    (Just (key, _modifiers), _time) -> moveBall . userInput key
+  where
+    userInput key s = case key of
+        KUp    -> over (leftPlayer . paddle) (subtract 1) s
+        KDown  -> over (leftPlayer . paddle) (+ 1) s
+        _other -> s
+
+    moveBall :: GameState -> GameState
+    moveBall s =
+        let Vec2Cart xPos yPos = view (ball . position) s
+            Vec2Rad magnitude angle = view (ball . velocity) s
+            newPos = Vec2Cart (xPos + magnitude * cos angle)
+                              (yPos + magnitude * sin angle)
+        in set (ball . position) newPos s
 
 main :: IO ()
-main = do
+main = withVty standardIOConfig (\vty -> do
 
-    cfg <- standardIOConfig
-    vty <- mkVty cfg
-    -- let line0 = string (defAttr ` withForeColor ` green) "first line"
-    --     line1 = string (defAttr ` withBackColor ` blue) "second line"
-    --     img = backgroundFill 30 20 <> line0 <-> line1
-    --     pic = picForImage img
-    let image = render initialGameState
-        pic = picForImage image
-    update vty pic
-    e <- nextEvent vty
-    shutdown vty
-    print ("Last event was: " ++ show e)
+    (fireVty, fireClock) <- do
+        (addVtyEvent, fireVty) <- newAddHandler
+        (addClockTickEvent, fireClock) <- newAddHandler
+        network <- compile (makeNetworkDescription vty addVtyEvent addClockTickEvent)
+        actuate network
+        pure (fireVty, fireClock)
 
-foo = do
+    withClock 30 (fireClock ()) (
+        fix (\loop -> nextEvent vty >>= \case
+            EvKey KEsc _ -> pure ()
+            EvKey (KChar 'q') _ -> pure ()
+            ev -> fireVty ev >> loop
+            ))
+    )
 
-
-    (addKeyEvent, fireKey) <- newAddHandler
-    (addClockTickEvent, fireClock) <- newAddHandler
-    network <- compile (makeNetworkDescription addKeyEvent addClockTickEvent)
-    actuate network
-
-    hSetEcho stdin False
-    hSetBuffering stdin NoBuffering
-    withClock 10 (fireClock ()) (
-        fix (\loop -> getChar >>= \case
-            'q' -> putStrLn "Quit"
-            key -> fireKey key >> loop ))
+withVty :: IO Config -> (Vty -> IO a) -> IO a
+withVty mkConfig = bracket (mkConfig >>= mkVty) shutdown
 
 withClock
     :: Int  -- ^ Ticks per second
     -> IO a -- ^ Action to run each tick
     -> IO b -- ^ Action to perform with the clock ticking
     -> IO b
-withClock ticksPerSecond tickAction body = do
-    thread <- async (forever (do
+withClock ticksPerSecond tickAction body = bracket acquire release (const body)
+  where
+    acquire = async (forever (do
         threadDelay (1e6 `quot` ticksPerSecond)
         tickAction ))
-    result <- body
-    cancel thread
-    pure result
+    release = cancel
 
 initialGameState :: GameState
 initialGameState = GameState
@@ -124,17 +138,28 @@ initialGameState = GameState
         { _score = 0
         , _paddle = 0 }
     , _ball = Ball
-        { _position = Vec2 0 0
-        , _velocity = Vec2 0 0 }
+        { _position = Vec2Cart 0 0
+        , _velocity = Vec2Rad 1 0 }
     }
 
-render :: GameState -> Image
-render gameState = horizJoin (translate 2      0 (renderPaddle (view leftPlayer gameState)))
-                             (translate (60-2) 0 (renderPaddle (view rightPlayer gameState)))
+render :: GameState -> Picture
+render gameState = picForLayers
+    [ renderBall (view ball gameState)
+    , renderLeftPlayer
+    , renderRightPlayer
+    ]
   where
+    renderLeftPlayer  = translate 2       0 (renderPaddle (view leftPlayer  gameState))
+    renderRightPlayer = translate (120-2) 0 (renderPaddle (view rightPlayer gameState))
+
     renderPaddle :: Player -> Image
     renderPaddle player = translate 0 yOffset paddleImage
       where
         style = mempty `withForeColor` white `withBackColor` black
         yOffset = view (paddle . to round) player
-        paddleImage = charFill style 'x' (1 :: Int) 5
+        paddleImage = charFill style 'x' (1 :: Int) 10 `horizJoin` charFill defAttr ' ' (1 :: Int) 10
+
+    renderBall :: Ball -> Image
+    renderBall b = translate (round (view (position . x) b))
+                             (round (view (position . y) b))
+                             (text' defAttr "o")
